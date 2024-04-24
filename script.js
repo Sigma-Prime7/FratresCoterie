9949d3e953edf0a1ed809a8276f34d65a85995be
R-level <47988410+R-level@users.noreply.github.com>
2024-04-24 17:15:04 +0200

    Enemy 1 animation
    Complete enemy 1 animation

--- script.js
+++ script.js
@@ -1,5 +1,57 @@
-const canvas= document.getElementById('#canvas1');
+/**@type {HTMLCanvasElement} */
+
+const canvas= document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-const img = new Image();
-img.src='assets\enemies\enemy1.png';
-ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+CANVAS_WIDTH = canvas.width = 500;
+CANVAS_HEIGHT = canvas.height = 700;
+const numberOfEnemies = 10;
+const enemiesArray = [];
+const enemyImage = new Image();
+enemyImage.src = "assets/enemies/enemy1.png";
+let gameFrame =0;
+class Enemy{
+    constructor() {
+        
+       this.image = new Image();
+       this.image.src ="assets/enemies/enemy1.png"
+        this.speed = Math.random() *4 -2   
+        this.spriteWidth=293;
+        this.spriteHeight=155;
+        this.width = this.spriteWidth/3;
+        this.height = this.spriteHeight/3;
+        this.frame=0;
+        this.flapSpeed= Math.floor(Math.random() * 3 + 1);
+        this.x = Math.random() * (canvas.width-this.width);
+        this.y= Math.random() * (canvas.height-this.height);
+    }
+    update(){
+        this.x+= Math.random() * 7 -3.5;
+        this.y+=Math.random() * 10 -5;
+        if(gameFrame % this.flapSpeed === 0){
+            this.frame > 4 ? this.frame =0:this.frame++;
+        }
+    }
+
+    draw(){
+        
+        ctx.drawImage(enemyImage,this.frame*this.spriteWidth,0,this.spriteWidth,this.spriteHeight,
+            this.x,this.y,this.width,this.height);
+    }
+
+};
+
+
+for(let i = 0; i < numberOfEnemies; i++){
+enemiesArray.push(new Enemy());
+}
+function animate(){
+    ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+    enemiesArray.forEach(enemy => {
+        enemy.update();
+        enemy.draw();
+        
+    })
+    gameFrame++;
+   requestAnimationFrame(animate);
+}
+animate();
