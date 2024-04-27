6e33a14a3fa7b7c7acdff197c22d84e5b99cb5ce
Sigma-Prime7 <102369437+Sigma-Prime7@users.noreply.github.com>
2024-04-27 19:32:22 +0200

    Progressing

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ const CANVAS_WIDTH = canvas.width=600;
 const CANVAS_HEIGHT= canvas.height=600;
 
 const playerImage = new Image();
+playerImage.src="assets/shadow_dog.png";
+const SPRITE_WIDTH = 575;
+const SPRITE_HEIGHT = 523;
+
+let frameX=0;
+let frameY=0;
+
+function animate(){
+    ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+   // ctx.fillRect(50,50,100,100);
+   //ctx.drawImage(image,sx,sy,sw,sh,dx,dy,dw,dh)
+    ctx.drawImage(playerImage,frameX*SPRITE_WIDTH,frameY*SPRITE_HEIGHT,SPRITE_WIDTH,SPRITE_HEIGHT,0,0,SPRITE_WIDTH,SPRITE_HEIGHT);
+    requestAnimationFrame(animate);
+}
+animate();
+
+
