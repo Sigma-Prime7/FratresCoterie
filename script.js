73bc80edf75294f440d9d184767c1bab24e4da31
Sigma-Prime7 <102369437+Sigma-Prime7@users.noreply.github.com>
2024-05-03 21:09:35 +0200

    Update Animation
    Added Key/Value pairs
    and assigned the animationStates to player State

--- script.js
+++ script.js
@@ -7,20 +7,51 @@ const playerImage = new Image();
 playerImage.src="assets/shadow_dog.png";
 const SPRITE_WIDTH = 575;
 const SPRITE_HEIGHT = 523;
-let frameX=0;
-let frameY=0;
+const playerState ='fall';
 let gameFrame = 0;
 const stggerFrame = 5;
 
-const SpriteAnimations =[];
+const spriteAnimations =[];
 const animationStates=[
     {
         name:'idle',
         frames: 7,
-    }
+    },
     {
         name:'jump',
         frames: 7,
+    },
+    {
+        name:'fall',
+        frames: 7,
+    },
+    {
+        name:'run',
+        frames: 9,
+    },
+    {
+        name:'dizzy',
+        frames: 11,
+    },
+    {
+        name:'sit',
+        frames: 5,
+    },
+    {
+        name:'roll',
+        frames: 5,
+    },
+    {
+        name:'bite',
+        frames: 5,
+    },
+    {
+        name:'ko',
+        frames: 12,
+    },
+    {
+        name:'getHit',
+        frames: 4,
     }
 ]
 animationStates.forEach((state,index) =>{
@@ -29,17 +60,20 @@ animationStates.forEach((state,index) =>{
     }
     for(let j=0; j<state.frames;j++){
         let positionX = j*SPRITE_WIDTH;
-        let poistionY=index*SPRITE_HEIGHT;
+        let positionY=index*SPRITE_HEIGHT;
         frames.loc.push({x:positionX,y:positionY});
     }
-    spriteAnimation[state.name] = frames;
+    spriteAnimations[state.name] = frames;
 });
-console.log(spriteAnimation);
+
+console.log(animationStates);
+
 function animate(){
     ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
-    let position =Math.floor(gameFrame/stggerFrame)%6;
-    frameX = SPRITE_WIDTH * position;
-    ctx.drawImage(playerImage,frameX,frameY*SPRITE_HEIGHT,SPRITE_WIDTH,SPRITE_HEIGHT,0,0,SPRITE_WIDTH,SPRITE_HEIGHT);
+    let position =Math.floor(gameFrame/stggerFrame)% spriteAnimations[playerState].loc.length;
+    let frameX = SPRITE_WIDTH * position;
+    let frameY = spriteAnimations[playerState].loc[position].y;
+    ctx.drawImage(playerImage,frameX,frameY,SPRITE_WIDTH,SPRITE_HEIGHT,0,0,SPRITE_WIDTH,SPRITE_HEIGHT);
 
 
     gameFrame++;
